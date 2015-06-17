fd3a07a590192bedca7a6c1430f33f41f27d20c7
Rod Woodman <roderickwoodman@gmail.com>
2015-06-16 18:11:19 -0700

    began twipper2.2 walkthrough

--- Twipper2.1/ViewController.swift
+++ Twipper2.1/ViewController.swift
@@ -8,15 +8,85 @@
 
 import UIKit
 import Foundation
+import Social
+import Accounts
 
 class ViewController: UITableViewController {
 
-    var tweets = [
-        Tweet(tweetText: "tweet one", userName: "userOne", createdAt: "Wed May 06", pictureURL: NSURL(string:"http://lorempixel.com/200/200/")),
-        Tweet(tweetText: "tweet two", userName: "userTwo", createdAt: "Wed May 06", pictureURL: NSURL(string: "http://lorempixel.com/201/201/")),
-        Tweet(tweetText: "tweet three", userName: "userTwo", createdAt: "Wed May 06", pictureURL: NSURL(string: "http://lorempixel.com/202/202/")),
-        Tweet(tweetText: "tweet four", userName: "userThree", createdAt: "Wed May 06", pictureURL: NSURL(string: "http://lorempixel.com/203/203/"))
-    ]
+    var tweets = [Tweet]()
+    
+    func requestTweets() {
+        let accountStore = ACAccountStore()
+        let twitterAccountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+        accountStore.requestAccessToAccountsWithType(twitterAccountType,
+            options: nil,
+            completion: {
+                (granted: Bool, error: NSError!) -> Void in
+                if (!granted) {
+                    println ("Access to Twitter Account denied")
+                } else {
+                    println("DBG requestTweets else1:")
+                    let twitterAccounts = accountStore.accountsWithAccountType(twitterAccountType)
+                    if twitterAccounts.count == 0 {
+                        println ("No Twitter Accounts available")
+                        return
+                    } else {
+                        println("DBG requestTweets else2:")
+
+                        let twitterParams = [
+                            "count" : "150"
+                        ]
+                        let twitterAPIURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+                        let request = SLRequest(forServiceType: SLServiceTypeTwitter,
+                            requestMethod: SLRequestMethod.GET,
+                            URL: twitterAPIURL,
+                            parameters: twitterParams)
+                        request.account = twitterAccounts.first as! ACAccount
+                        request.performRequestWithHandler ( {
+                            (data: NSData!, urlResponse: NSHTTPURLResponse!, error: NSError!) -> Void in
+                            self.handleTweetsResponse(data, urlResponse: urlResponse, error: error)
+                        })
+                    }
+                }
+        })
+    }
+    
+    func handleTweetsResponse(data: NSData!, urlResponse: NSHTTPURLResponse!, error: NSError!) {
+        if let dataValue = data {
+            var parseError : NSError? = nil
+            let jsonObject : AnyObject? =
+            NSJSONSerialization.JSONObjectWithData(dataValue,
+                options: NSJSONReadingOptions(0),
+                error: &parseError)
+            if parseError != nil {
+                return
+            }
+            if let jsonArray = jsonObject as? [[String: AnyObject]] {
+                self.tweets.removeAll(keepCapacity: true)
+                for tweetDict in jsonArray {
+                    let tweetText = tweetDict["text"] as! String
+                    let df = NSDateFormatter()
+                    // this is the format that we are getting from Twitter API
+                    df.dateFormat = "EEE MMM d HH:mm:ss Z y"
+                    let createdAtLong = tweetDict["created_at"] as! String
+                    // convert a string into NSDate using our date formatter
+                    let createdAtShort = df.dateFromString(createdAtLong)
+                    // configure our date formatter to have a shorter format
+                    df.dateFormat = "EEE MMM d"
+                    // use our newly configured date formatter to convert string to NSDate
+                    let createdAt = df.stringFromDate(createdAtShort!)
+                    let userDict = tweetDict["user"] as! NSDictionary
+                    let userName = userDict["name"] as! String
+                    let pictureURL = userDict["profile_image_url"] as! String
+                    let tweet = Tweet(tweetText: tweetText, userName: userName, createdAt: createdAt, pictureURL: NSURL(string: pictureURL))
+                    self.tweets.append(tweet)
+                }
+                self.tableView.reloadData()
+            }
+        } else {
+            println ("handleTwitterData received no data")
+        }
+    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let dequeued: AnyObject = tableView.dequeueReusableCellWithIdentifier("TweetCell", forIndexPath: indexPath)
@@ -43,6 +113,7 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestTweets()
         // Do any additional setup after loading the view, typically from a nib.
 //        tableView.delegate = self
     }
