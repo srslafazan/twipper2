646a506397b9adc89f0df98af4a82918ca54a31f
Shain Lafazan <srslafazan@gmail.com>
2015-06-16 18:35:55 -0700

    UI tableView.reloadData is now on Main Thread

--- Twipper2.1/ViewController.swift
+++ Twipper2.1/ViewController.swift
@@ -25,14 +25,11 @@ class ViewController: UITableViewController {
                 if (!granted) {
                     println ("Access to Twitter Account denied")
                 } else {
-                    println("DBG requestTweets else1:")
                     let twitterAccounts = accountStore.accountsWithAccountType(twitterAccountType)
                     if twitterAccounts.count == 0 {
                         println ("No Twitter Accounts available")
                         return
                     } else {
-                        println("DBG requestTweets else2:")
-
                         let twitterParams = [
                             "count" : "150"
                         ]
@@ -52,40 +49,45 @@ class ViewController: UITableViewController {
     }
     
     func handleTweetsResponse(data: NSData!, urlResponse: NSHTTPURLResponse!, error: NSError!) {
-        if let dataValue = data {
-            var parseError : NSError? = nil
-            let jsonObject : AnyObject? =
-            NSJSONSerialization.JSONObjectWithData(dataValue,
-                options: NSJSONReadingOptions(0),
-                error: &parseError)
-            if parseError != nil {
-                return
-            }
-            if let jsonArray = jsonObject as? [[String: AnyObject]] {
-                self.tweets.removeAll(keepCapacity: true)
-                for tweetDict in jsonArray {
-                    let tweetText = tweetDict["text"] as! String
-                    let df = NSDateFormatter()
-                    // this is the format that we are getting from Twitter API
-                    df.dateFormat = "EEE MMM d HH:mm:ss Z y"
-                    let createdAtLong = tweetDict["created_at"] as! String
-                    // convert a string into NSDate using our date formatter
-                    let createdAtShort = df.dateFromString(createdAtLong)
-                    // configure our date formatter to have a shorter format
-                    df.dateFormat = "EEE MMM d"
-                    // use our newly configured date formatter to convert string to NSDate
-                    let createdAt = df.stringFromDate(createdAtShort!)
-                    let userDict = tweetDict["user"] as! NSDictionary
-                    let userName = userDict["name"] as! String
-                    let pictureURL = userDict["profile_image_url"] as! String
-                    let tweet = Tweet(tweetText: tweetText, userName: userName, createdAt: createdAt, pictureURL: NSURL(string: pictureURL))
-                    self.tweets.append(tweet)
+        dispatch_async(dispatch_get_main_queue(), {() -> Void in
+            if let dataValue = data {
+                var parseError : NSError? = nil
+                let jsonObject : AnyObject? =
+                NSJSONSerialization.JSONObjectWithData(dataValue,
+                    options: NSJSONReadingOptions(0),
+                    error: &parseError)
+                if parseError != nil {
+                    return
                 }
-                self.tableView.reloadData()
+
+                if let jsonArray = jsonObject as? [[String: AnyObject]] {
+
+                    self.tweets.removeAll(keepCapacity: true)
+                    for tweetDict in jsonArray {
+                        let tweetText = tweetDict["text"] as! String
+                        let df = NSDateFormatter()
+                        // this is the format that we are getting from Twitter API
+                        df.dateFormat = "EEE MMM d HH:mm:ss Z y"
+                        let createdAtLong = tweetDict["created_at"] as! String
+                        // convert a string into NSDate using our date formatter
+                        let createdAtShort = df.dateFromString(createdAtLong)
+                        // configure our date formatter to have a shorter format
+                        df.dateFormat = "EEE MMM d"
+                        // use our newly configured date formatter to convert string to NSDate
+                        let createdAt = df.stringFromDate(createdAtShort!)
+                        let userDict = tweetDict["user"] as! NSDictionary
+                        let userName = userDict["name"] as! String
+                        let pictureURL = userDict["profile_image_url"] as! String
+                        let tweet = Tweet(tweetText: tweetText, userName: userName, createdAt: createdAt, pictureURL: NSURL(string: pictureURL))
+                        self.tweets.append(tweet)
+                    }
+                    println (NSThread.isMainThread() ? "Main Thread" : "Not on Main Thread")
+                    self.tableView.reloadData()
+                }
+            } else {
+                println ("handleTwitterData received no data")
             }
-        } else {
-            println ("handleTwitterData received no data")
-        }
+        })
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -115,16 +117,10 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         requestTweets()
         // Do any additional setup after loading the view, typically from a nib.
-//        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    // Display customization
-    
-
 }
